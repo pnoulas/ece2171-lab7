152ff82b5d16a2394c58a5970bcd50ae102cbc50
pnoulas <229993270+pnoulas@users.noreply.github.com>
2025-12-10 20:49:43 -0500

    Update and rename lab7b.cpp to lab7bversion1.cpp

---
+++ lab7bversion1.cpp
@@ -0,0 +1,23 @@
+/* 
+ * Project Lab 7b
+ * Author: Paulina Noulas
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+#include "Particle.h"
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+void setup() {
+  Serial1.begin(9600, SERIAL_8E1);
+}
+
+void loop() {
+  Serial1.print("VU");
+
+  delay(200);
+}
