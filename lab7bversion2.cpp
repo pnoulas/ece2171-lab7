e60ffde49d2a75b2bbc471a6771e6e138dbcf8f1
pnoulas <229993270+pnoulas@users.noreply.github.com>
2025-12-10 20:51:36 -0500

    Update lab7bversion2.cpp

--- lab7bversion2.cpp
+++ lab7bversion2.cpp
@@ -1,1 +1,25 @@
+/* 
+ * Project Lab 7b version 2
+ * Author: Paulina Noulas
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
 
+#include "Particle.h"
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+int16_t x = 27099;
+
+void setup() {
+  Serial1.begin(9600, SERIAL_8E1);
+}
+
+void loop() {
+  Serial1.println(x);
+
+  delay(200);
+}
